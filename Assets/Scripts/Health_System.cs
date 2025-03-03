081ba62469a80f7b58928a2ca9a72946989a06a3
Stefan Mandita <damsteffy2000@gmail.com>
2025-03-03 20:15:30 +0100

    Simple health system

---
+++ Assets/Scripts/Health_System.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Health_System : MonoBehaviour
+{
+    public float max_health = 200f;
+    private float current_health;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        current_health = max_health;
+    }
+    public void Deal_Damage(float damage)
+    {
+        current_health -= max_health;
+        Debug.Log(gameObject.name + "took damage equal to:" + damage + ". Updated hp: " + current_health);
+
+        if(current_health <=0)
+        {
+            Dead();
+        }
+    }
+    public void Dead()
+    {
+        Debug.Log(gameObject.name + "was destroyed");
+        Destroy(gameObject);
+    }
+    public void heal(float heal_amount)
+    {
+        current_health += heal_amount;
+        if(current_health>max_health)
+        {
+            current_health = max_health;
+        }
+        Debug.Log(gameObject.name + "was healed:" + heal_amount + ". Updated hp:" + current_health);
+    }
+}
