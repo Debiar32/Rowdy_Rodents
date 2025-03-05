fc7aa0e3635cb5776411e47c7f27f14cd41f5e87
Debiar32 <egemen.filiz006@gmail.com>
2025-03-05 13:23:18 +0100

    Revert "DASHING_IS_FIXED"
    This reverts commit 551f94287a3b20dc94c2febb8e65b1176c72ace5.
    
    Reapply "DASHING_IS_FIXED"
    
    This reverts commit 236aac85465856eb5fbd85f1a9533beab3185076.
    
    DASHING_IS_FIXED
    
    DASHING_IS_FIXED

--- Assets/Scripts/Player_Attack_Manager.cs
+++ Assets/Scripts/Player_Attack_Manager.cs
@@ -2,6 +2,22 @@ using UnityEngine;
 
 public class Player_Attack_Manager : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
+    [SerializeField] private Transform Attack_ref;
+    [SerializeField] private Player_Movement player_movement;
+
+    public enum Attack_States
+    {
+        Idle,
+        Basic_Slash_Glave,
+        Heavy_Strike_Glave,
+        Dash_Strike_Glave,
+        Spin_Galve_Attack
+
+    }
+    public Attack_States Current_Attack_State = Attack_States.Idle;
+>>>>>>> Stashed changes
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
