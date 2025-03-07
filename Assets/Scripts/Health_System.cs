84e8d618e053f257bb278e774dd628dc06a03e6e
Lars <ReparableJaguarplays@gmail.com>
2025-03-07 10:54:11 +0100

    Update #01

--- Assets/Scripts/Health_System.cs
+++ Assets/Scripts/Health_System.cs
@@ -1,21 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health_System : MonoBehaviour
 {
+    [Header("Health Bar")]
+    public Slider slider;
+
+    [Header("Bubble Effect")]
+    public GameObject bubbleEffectPrefab;
+    public Transform effectSpawnPoint;
+    private Vector3 lastSpawnPosition;
+
+    [Header("Misc")]
     public float max_health = 200f;
     private float current_health;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         current_health = max_health;
+        UpdateHealthBar();
+    }
+
+    private void Update()
+    {
+        // Update the last known position of the spawn point.
+        lastSpawnPosition = effectSpawnPoint.position;
     }
     public void Deal_Damage(float damage)
     {
         current_health -= damage;
+        SpawnBubbles();
         Debug.Log(gameObject.name + "took damage equal to:" + damage + ". Updated hp: " + current_health);
+
+        UpdateHealthBar();
 
         if(current_health <=0)
         {
+            SpawnBubbles();
             Dead();
         }
     }
@@ -23,6 +44,7 @@ public class Health_System : MonoBehaviour
     {
         Debug.Log(gameObject.name + "was destroyed");
         Destroy(gameObject);
+
     }
     public void heal(float heal_amount)
     {
@@ -32,5 +54,32 @@ public class Health_System : MonoBehaviour
             current_health = max_health;
         }
         Debug.Log(gameObject.name + "was healed:" + heal_amount + ". Updated hp:" + current_health);
+
+        UpdateHealthBar();
     }
+
+    public void UpdateHealthBar()
+    {
+        if (slider != null)
+        {
+            slider.maxValue = max_health;
+            slider.value = current_health;
+        }
+    }
+
+    private void SpawnBubbles()
+    {
+        if (bubbleEffectPrefab != null && effectSpawnPoint != null)
+        {
+            // Instantiate the bubble effect at the spawn point
+            Instantiate(bubbleEffectPrefab, lastSpawnPosition, Quaternion.identity);
+        }
+        else
+        {
+            // If bubbleEffectPrefab is null, destroy the GameObject this script is attached to
+            Destroy(gameObject);
+        }
+    }
+
+
 }
